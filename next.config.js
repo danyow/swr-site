7743ce773e34b7d0ab49db86db7d8ccc22a01f08
Danyow <i.zuucol@gmail.com>
2021-12-19 16:57:32 +0800

    配置项提交

--- next.config.js
+++ next.config.js
@@ -1,62 +1,57 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.js",
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
   unstable_stork: false,
-  unstable_staticImage: true,
-});
+  unstable_staticImage: true
+})
 
 module.exports = withNextra({
   i18n: {
-    locales: ["en-US", "zh-CN", "es-ES", "ja", "ko", "ru"],
-    defaultLocale: "en-US",
+    locales: ['zh-CN'],
+    defaultLocale: 'zh-CN'
   },
   redirects: () => {
     return [
+      // {
+      //   source: '/docs',
+      //   destination: '/docs/getting-started',
+      //   statusCode: 301
+      // },
+      // {
+      //   source: '/advanced/performance',
+      //   destination: '/docs/advanced/performance',
+      //   statusCode: 301
+      // },
+      // {
+      //   source: '/advanced/cache',
+      //   destination: '/docs/advanced/cache',
+      //   statusCode: 301
+      // },
+      // {
+      //   source: '/docs/cache',
+      //   destination: '/docs/advanced/cache',
+      //   statusCode: 301
+      // },
+      // {
+      //   source: '/change-log',
+      //   destination: '/docs/change-log',
+      //   statusCode: 301
+      // },
+      // {
+      //   source: '/blog',
+      //   destination: '/blog',
+      //   statusCode: 302
+      // },
+      // {
+      //   source: '/docs',
+      //   destination: '/docs/getting-started',
+      //   statusCode: 302
+      // },
       {
-        source: "/docs",
-        destination: "/docs/getting-started",
-        statusCode: 301,
-      },
-      {
-        source: "/advanced/performance",
-        destination: "/docs/advanced/performance",
-        statusCode: 301,
-      },
-      {
-        source: "/advanced/cache",
-        destination: "/docs/advanced/cache",
-        statusCode: 301,
-      },
-      {
-        source: "/docs/cache",
-        destination: "/docs/advanced/cache",
-        statusCode: 301,
-      },
-      {
-        source: "/change-log",
-        destination: "/docs/change-log",
-        statusCode: 301,
-      },
-      {
-        source: "/blog/swr-1",
-        destination: "/blog/swr-v1",
-        statusCode: 301,
-      },
-      {
-        source: "/blog",
-        destination: "/blog/swr-v1",
-        statusCode: 302,
-      },
-      {
-        source: "/docs",
-        destination: "/docs/getting-started",
-        statusCode: 302,
-      },
-      {
-        source: "/examples",
-        destination: "/examples/basic",
-        statusCode: 302,
-      },
-    ];
-  },
-});
+        source: '/examples',
+        destination: '/examples/basic',
+        statusCode: 302
+      }
+    ]
+  }
+})
--- theme.config.js
+++ theme.config.js
@@ -1,241 +1,153 @@
-import { useRouter } from "next/router";
+// noinspection HtmlUnknownTarget,JSUnresolvedLibraryURL
+
+import { useRouter } from 'next/router'
 
 const Logo = ({ height }) => (
-  <svg height={height} viewBox="0 0 291 69" fill="none">
+  <svg height={height} viewBox='0 0 69 69' fill='none'>
     <path
-      d="M0 36.53c.07 17.6 14.4 32.01 32.01 32.01a32.05 32.05 0 0032.01-32V32a13.2 13.2 0 0123.4-8.31h20.7A32.07 32.07 0 0077.2 0a32.05 32.05 0 00-32 32.01v4.52A13.2 13.2 0 0132 49.71a13.2 13.2 0 01-13.18-13.18 3.77 3.77 0 00-3.77-3.77H3.76A3.77 3.77 0 000 36.53zM122.49 68.54a32.14 32.14 0 01-30.89-23.7h20.67a13.16 13.16 0 0023.4-8.3V32A32.05 32.05 0 01167.68 0c17.43 0 31.64 14 32 31.33l.1 5.2a13.2 13.2 0 0023.4 8.31h20.7a32.07 32.07 0 01-30.91 23.7c-17.61 0-31.94-14.42-32.01-32l-.1-4.7v-.2a13.2 13.2 0 00-13.18-12.81 13.2 13.2 0 00-13.18 13.18v4.52a32.05 32.05 0 01-32.01 32.01zM247.94 23.7a13.16 13.16 0 0123.4 8.31 3.77 3.77 0 003.77 3.77h11.3a3.77 3.77 0 003.76-3.77A32.05 32.05 0 00258.16 0a32.07 32.07 0 00-30.92 23.7h20.7z"
-      fill="currentColor"
+      d='M29.17,10.15c-1.33,0.9-0.69,2.29-0.95,3.55c-0.13,0.63-0.73,0.98-0.91,1.59c-0.34,1.15-0.09,3.71,0.56,4.73 c0.59,0.94,3.09,0.98,3.03,2.22c-0.04,0.7-0.74,1.36-1.07,1.95c-0.8,1.42-1.7,3.1-2.27,4.62c-0.56,1.47-0.77,2.94-1.15,4.44 c-0.94,3.77-1.67,7.45-1.3,11.37c0.1,1.01,0.74,1.49,1.21,2.31c0.38,0.66,0.56,1.55,0.67,2.31c0.13,0.93,0.05,1.91,0.05,2.84 c0,0.41-0.07,0.97,0.18,1.33c0.57,0.8,2.02,0.34,2.71,1.07c0.8,0.84,0.84,2.41,1.19,3.46c0.22,0.66,0.47,1.34,0.88,1.9 c0.37,0.5,0.92,0.87,1.14,1.47c0.18,0.49,0.11,1.09,0.11,1.6h3.91c0.43,0,1.11,0.1,1.42-0.28c0.4-0.51,0.09-1.45,0.04-2.03 c-0.06-0.59,0.01-1.19-0.14-1.78c-0.33-1.3-1.09-2.44-1.42-3.73c-0.16-0.6-0.38-1.34-0.06-1.92c0.42-0.76,1.42-1.08,1.57-2 c0.24-1.47-1.18-2.86-1.57-4.25c0.69-0.23,1.38-0.48,1.83-1.09c0.34-0.45,0.42-1.05,0.89-1.4c1.01-0.76,2.15-0.65,2.98-1.79 c1.34-1.84,1.71-4.17,1.95-6.38c0.34-3.06,0.4-7.07-1.35-9.77c-0.57-0.87-1.44-1.12-2.12-1.83c-0.66-0.69-0.96-1.48-1.87-1.92 c-0.99-0.48-1.97-0.41-3.02-0.52c1.09-0.57,2.31-0.79,3.36-1.48c1.4-0.91,2-2.46,2.33-4.03c1.02,0.48,4.1,2.05,4.04-0.35 c-0.03-1.28-2.47-1.33-3.17-2.25c-0.61-0.79-0.46-2.15-0.94-3.09c-1.44-2.84-4.49-4.18-7.57-3.95C32.56,7.22,29.27,7.87,29.17,10.15z'
+      fill='currentColor'
     />
   </svg>
-);
+)
 
 const Vercel = ({ height = 20 }) => (
-  <svg height={height} viewBox="0 0 283 64" fill="none">
+  <svg height={height} viewBox='0 0 283 64' fill='none'>
     <path
-      fill="currentColor"
-      d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z"
+      fill='currentColor'
+      d='M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z'
     />
   </svg>
-);
+)
 
 const TITLE_WITH_TRANSLATIONS = {
-  "en-US": "React Hooks for Data Fetching",
-  "zh-CN": "用于数据请求的 React Hooks 库",
-  "es-ES": "Biblioteca React Hooks para la obtención de datos",
-  ja: "データ取得のための React Hooks ライブラリ",
-  ko: "데이터 가져오기를 위한 React Hooks",
-  ru: "React хуки для выборки данных",
-};
+  'zh-CN': 'string all'
+}
 
 export default {
-  github: "https://github.com/vercel/swr",
-  docsRepositoryBase: "https://github.com/vercel/swr-site/blob/master/pages",
-  titleSuffix: " – SWR",
+  github: 'https://github.com/danyow',
+  docsRepositoryBase: 'https://github.com/danyow/danyow_doc',
+  titleSuffix: ' – danyow',
   search: true,
   unstable_stork: false,
   floatTOC: true,
   logo: () => {
-    const { locale } = useRouter();
+    const { locale } = useRouter()
     return (
       <>
         <Logo height={18} />
-        <span className="mx-2 font-extrabold hidden md:inline select-none">
-          SWR
+        <span className='mx-2 font-extrabold hidden md:inline select-none'>
+          danyow
         </span>
-        <span className="text-gray-600 font-normal hidden lg:!inline whitespace-no-wrap">
+        <span className='text-gray-600 font-normal hidden lg:!inline whitespace-no-wrap'>
           {TITLE_WITH_TRANSLATIONS[locale]}
         </span>
       </>
-    );
+    )
   },
   head: ({ title, meta }) => {
     return (
       <>
         {/* Favicons, meta */}
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicon/apple-touch-icon.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon/favicon-32x32.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon/favicon-16x16.png'
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel='manifest' href='/favicon/site.webmanifest' />
         <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
+          rel='mask-icon'
+          href='/favicon/safari-pinned-tab.svg'
+          color='#000000'
         />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='theme-color' content='#ffffff' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='Content-Language' content='en' />
         <meta
-          name="description"
+          name='description'
           content={
             meta.description ||
-            "SWR is a React Hooks library for data fetching. SWR first returns the data from cache (stale), then sends the fetch request (revalidate), and finally comes with the up-to-date data again."
+            'SWR is a React Hooks library for data fetching. SWR first returns the data from cache (stale), then sends the fetch request (revalidate), and finally comes with the up-to-date data again.'
           }
         />
         <meta
-          name="og:description"
+          name='og:description'
           content={
             meta.description ||
-            "SWR is a React Hooks library for data fetching. SWR first returns the data from cache (stale), then sends the fetch request (revalidate), and finally comes with the up-to-date data again."
+            'SWR is a React Hooks library for data fetching. SWR first returns the data from cache (stale), then sends the fetch request (revalidate), and finally comes with the up-to-date data again.'
           }
         />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@vercel" />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@vercel' />
         <meta
-          name="twitter:image"
+          name='twitter:image'
           content={
             meta.image ||
-            "https://assets.vercel.com/image/upload/v1572282926/swr/twitter-card.jpg"
+            'https://assets.vercel.com/image/upload/v1572282926/swr/twitter-card.jpg'
           }
         />
         <meta
-          name="og:title"
+          name='og:title'
           content={
-            title ? title + " – SWR" : "SWR: React Hooks for Data Fetching"
+            title ? title + ' – SWR' : 'SWR: React Hooks for Data Fetching'
           }
         />
         <meta
-          name="og:image"
+          name='og:image'
           content={
             meta.image ||
-            "https://assets.vercel.com/image/upload/v1572282926/swr/twitter-card.jpg"
+            'https://assets.vercel.com/image/upload/v1572282926/swr/twitter-card.jpg'
           }
         />
-        <meta name="apple-mobile-web-app-title" content="SWR" />
+        <meta name='apple-mobile-web-app-title' content='SWR' />
         <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
-          media="print"
+          rel='stylesheet'
+          href='https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css'
+          media='print'
           onLoad="this.media='all'"
         />
       </>
-    );
+    )
   },
   footerEditLink: ({ locale }) => {
     switch (locale) {
-      case "zh-CN":
-        return "在 GitHub 上编辑本页";
-      case "es-ES":
-        return "Edite esta página en GitHub";
-      case "ja":
-        return "Github で編集する";
-      case "ko":
-        return "Github에서 이 페이지 편집하기";
-      case "ru":
-        return "Редактировать на GitHub";
       default:
-        return "Edit this page on GitHub";
+        return '在 GitHub 上编辑本页'
     }
   },
   footerText: ({ locale }) => {
     switch (locale) {
-      case "zh-CN":
+      default:
         return (
           <a
-            href="https://vercel.com/?utm_source=swr_zh-cn"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
+            href='https://vercel.com/?utm_source=swr_zh-cn'
+            target='_blank'
+            rel='noopener'
+            className='inline-flex items-center no-underline text-current font-semibold'
           >
-            <span className="mr-2">由</span>
-            <span className="mr-2">
+            <span className='mr-2'>由</span>
+            <span className='mr-2'>
               <Vercel />
             </span>
             驱动
           </a>
-        );
-      case "es-ES":
-        return (
-          <a
-            href="https://vercel.com/?utm_source=swr_es-es"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-2">Desarrollado por</span>
-            <span className="mr-2">
-              <Vercel />
-            </span>
-          </a>
-        );
-      case "ja":
-        return (
-          <a
-            href="https://vercel.com/?utm_source=swr_ja"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-2">提供</span>
-            <span className="mr-2">
-              <Vercel />
-            </span>
-          </a>
-        );
-      case "ko":
-        return (
-          <a
-            href="https://vercel.com/?utm_source=swr_ko"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-2">Powered by</span>
-            <span className="mr-2">
-              <Vercel />
-            </span>
-          </a>
-        );
-      case "ru":
-        return (
-          <a
-            href="https://vercel.com/?utm_source=swr_ru"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-2">Работает на</span>
-            <span className="mr-2">
-              <Vercel />
-            </span>
-          </a>
-        );
-      default:
-        return (
-          <a
-            href="https://vercel.com/?utm_source=swr"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-1">Powered by</span>
-            <span>
-              <Vercel />
-            </span>
-          </a>
-        );
+        )
     }
   },
   i18n: [
-    { locale: "en-US", text: "English" },
-    { locale: "es-ES", text: "Español" },
-    { locale: "zh-CN", text: "简体中文" },
-    { locale: "ja", text: "日本語" },
-    { locale: "ko", text: "한국어" },
-    { locale: "ru", text: "Русский" },
-  ],
-};
+    { locale: 'zh-CN', text: '简体中文' }
+  ]
+}
